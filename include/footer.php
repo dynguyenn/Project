10916312ba8ad79297b78dc9071068d46a97c9e8
dynguyenn <nnguyen.duy2111@gmail.com>
2024-03-11 18:33:57 +0700

    init project

---
+++ include/footer.php
@@ -0,0 +1,64 @@
+
+	<!-- footer -->
+	<footer>
+		<div class="footer-top-first">
+			<div class="container py-md-5 py-sm-4 py-3">
+				<!-- footer first section -->
+				<h2 class="footer-top-head-w3l font-weight-bold mb-2">Giới thiệu về chúng tôi</h2>
+				<p class="footer-main mb-4">
+				Nếu bạn đang cân nhắc mua máy tính xách tay mới, tìm kiếm dàn âm thanh nổi mới mạnh mẽ cho ô tô hoặc mua HDTV mới, 
+				chúng tôi giúp bạn dễ dàng tìm thấy chính xác những gì bạn cần với mức giá bạn có thể mua được.
+				Chúng tôi cung cấp Giá thấp hàng ngày trên TV, máy tính xách tay,
+				 điện thoại di động, máy tính bảng và iPad, trò chơi điện tử, máy tính để bàn, máy ảnh và máy quay phim, âm thanh, video, v.v.</p>
+				<!-- //footer first section -->
+				<!-- footer second section -->
+				<div class="row w3l-grids-footer border-top border-bottom py-sm-4 py-3">
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-dolly"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Miễn phí vận chuyển</h3>
+								<p>Đơn hàng trên 100$</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer my-md-0 my-4">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-shipping-fast"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Vận chuyển nhanh</h3>
+								<p>Toàn quốc</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="far fa-thumbs-up"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Tin cậy</h3>
+								<p>Sự đảm bảo</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- //footer second section -->
+			</div>
+		</div>
+		
+		<!-- //footer third section -->
+		
+	</footer>
+	<!-- //footer -->
+	<!-- copyright -->
+	<div class="copy-right py-3">
+		<div class="container">
+			<p class="text-center text-white">© 2022 Lập Trình Web Nâng Cao</p>
+		</div>
+	</div>
+	<!-- //copyright -->
